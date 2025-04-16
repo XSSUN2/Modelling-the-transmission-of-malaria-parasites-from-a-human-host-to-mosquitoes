1a50f85c761dab5522f9e6829b04acf30db91167
XSSUN2 <65107808+XSSUN2@users.noreply.github.com>
2025-04-16 15:54:41 +1000

    Add files via upload

--- ABC-SMC_vector.R
+++ ABC-SMC_vector.R
@@ -1,5 +1,7 @@
-##Parameter estimation
 transmission_model <- function(N, Gm, Gf, V, rho, r, alpha_go, beta_go, alpha_os, beta_os){
+  a <- 0.0050300 
+  b <- 0.1326802
+  c <- 0.3272420
   record <- matrix(0, N, 7)
   M <- rbinom(N,Gm,V/1000)
   F <- rbinom(N,Gf,V/1000)
@@ -8,7 +10,7 @@ transmission_model <- function(N, Gm, Gf, V, rho, r, alpha_go, beta_go, alpha_os
   for (i in 1:N) {
     n[i] <- rbinom(1,round(min(rho*M[i],F[i]),0),r)
     p <- runif(1,0,1)
-    t[i] <- uniroot.all(function(x){1-p-exp(-0.0043140*x*exp(0.0874910*x))},c(0,100))
+    t[i] <- 1/b*log(1+log(p)/(-a/b))
   }
   record[,c(1,4,5,6)] <- c(n,t,M,F)
   for (i in 1:N) {
@@ -16,7 +18,7 @@ transmission_model <- function(N, Gm, Gf, V, rho, r, alpha_go, beta_go, alpha_os
       oocy <- rgamma(n[i],alpha_go,beta_go)
       sporo <- rgamma(n[i],alpha_os,beta_os)
       p <- runif(1,0,1)
-      t <- uniroot.all(function(x){1-p-exp(-0.0043140*x*exp(0.0874910*x+0.3604109))},c(0,100))
+      t <- 1/b*log(1+log(p)/(-a/b*exp(c)))
       record[i,2:4] <- c(min(oocy),min(oocy+sporo),t)
       record[i,7] <- max(oocy+sporo)
     }
@@ -30,13 +32,13 @@ oocyst_prev <- function(N, Gm, Gf, V, rho, r, alpha_go, beta_go, alpha_os, beta_
   survival <- matrix(0,N,240)
   for (i in 1:N) {
     if (ceiling(a[i,4]*24)<240){
-      survival[i,(ceiling(a[i,4])*24):240] <- 1
+      survival[i,(ceiling(a[i,4]*24)):240] <- 1
     }
   }
   oocyst <- matrix(0,N,240)
   for (i in 1:N) {
     if (ceiling(a[i,2]*24)<240 & a[i,2]!=0 ){
-      oocyst[i, (ceiling(a[i,2]*24)):(ceiling(a[i,7]*24))] <- 1
+      oocyst[i, (ceiling(a[i,2]*24)):min((ceiling(a[i,7]*24)),240)] <- 1
     }
   }
   prev_oocyst <- vector()
@@ -45,6 +47,8 @@ oocyst_prev <- function(N, Gm, Gf, V, rho, r, alpha_go, beta_go, alpha_os, beta_
   }
   return(prev_oocyst)
 }
+simulation <- oocyst_prev(30,round(1976.8,0),round( 4679.3,0),0.87,2.55,0.6,13.6, 2.7, 17.8, 2.2)
+simulation[8.5*24]
 
 
 library(foreach)
@@ -53,7 +57,7 @@ library(mvtnorm)
 cl <- parallel::makeCluster(8)
 doParallel::registerDoParallel(cl)
 parallel::stopCluster(cl)
-
+library(rootSolve)
 
 time.step=20
 n=1000
@@ -65,20 +69,21 @@ para <- list()
 epsilon <- rep(0,time.step)
 epsilon[1] <- 0.3699087
 
-for (t in 1:14){
+
+for (t in 2:14){
   if(t==1){
     rec <- matrix(0,n,4)
-    result <- foreach(i=1:n,.packages=c("tmvtnorm","rootSolve")) %dopar% {
+    result <- foreach(i=1:n,.packages=c("tmvtnorm")) %dopar% {
       d <- epsilon[t]+1
       time <- 0
       while(d > epsilon[t]){
         set.seed(n*time+i)
-        proposed.mu <- c(runif(1,0,8),runif(1,0,1))
+        proposed.mu <- c(runif(1,1,8),runif(1,0,1))
         #simulation <- try(sto_model(proposed.mu[1]*4000,proposed.mu[2],proposed.mu[3],proposed.mu[4],kd(pk101,proposed.mu[5],proposed.mu[6],proposed.mu[7]),proposed.mu[8],proposed.mu[9],proposed.mu[10],proposed.mu[11],proposed.mu[12],42,25,672),silent=T)
         oocyst_record <- vector()
         for (j in 1:33){
-          simulation <- oocyst_prev(30,round(as.numeric(data[j,2]),0),round(as.numeric(data[j,3]),0),0.87,proposed.mu[1],proposed.mu[2],13.6, 2.7, 17.8, 2.2)
-          oocyst_record[j] <- simulation[85]
+          simulation <- oocyst_prev(30,round(as.numeric(data[j,2]),0),round(as.numeric(data[j,3]),0),0.87,2.55,0.6,13.6, 2.7, 17.8, 2.2)
+          oocyst_record[j] <- simulation[8.5*24]
         }
         time <- time+1
         d <- sqrt(sum((oocyst_record-as.numeric(data[,4])/as.numeric(data[,5]))^2))
@@ -121,21 +126,20 @@ for (t in 1:14){
         sample.particle <- sample(n,1,prob=weights[t-1,])
         proposed.mu0 <- para[[t-1]][sample.particle,]
         set.seed(n*time+i)
-        proposed.mu <- as.numeric(rtmvnorm(1,proposed.mu0,cov_matr,c(0,0) ,c(8,1)))
+        proposed.mu <- as.numeric(rtmvnorm(1,proposed.mu0,cov_matr,c(1,0) ,c(8,1)))
         oocyst_record <- vector()
         for (j in 1:33){
           simulation <- oocyst_prev(30,round(as.numeric(data[j,2]),0),round(as.numeric(data[j,3]),0),0.87,proposed.mu[1],proposed.mu[2],13.7, 2.7, 17.8, 2.2)
-          oocyst_record[j] <- simulation[85]
+          oocyst_record[j] <- simulation[8.5*24]
         }
         time <- time+1
         d <- sqrt(sum((oocyst_record-as.numeric(data[,4])/as.numeric(data[,5]))^2))
-        #d <- sqrt(sum((oocyst_record-as.numeric(data[,4])/as.numeric(data[,5]))^2))
       }
       mu.weights.denominator=0
       for (k in 1:n){
         mu.weights.denominator <- mu.weights.denominator+weights[t-1,k]*dtmvnorm(proposed.mu,as.numeric(para[[t-1]][k,]),cov_matr,c(0,0) ,c(8,1))
       }
-      mu.weights.numerator = prod(dunif(proposed.mu,c(0,0) ,c(8,1)))
+      mu.weights.numerator = prod(dunif(proposed.mu,c(1,0) ,c(8,1)))
       weights_current = mu.weights.numerator/mu.weights.denominator
       c(proposed.mu,d,time,weights_current)
     }
@@ -148,21 +152,26 @@ for (t in 1:14){
     weights[t,] <- rec[,5]
     weights[t,] <- weights[t,]/sum(weights[t,])
   }
+  print(t)
 }
-result <- as.data.frame(para[[14]])
+
+result <- as.data.frame(para[[13]])
 names(result) <- c("rho","p_f")
 
-###Figure 4
-hist(para[[14]][,1],breaks=50,main="",xlab=expression(rho),cex.axis=2,cex.lab=2,yaxt="n")
+#hist
+hist(para[[13]][,1],breaks=50,main="",xlab=expression(rho),cex.axis=2,cex.lab=2,yaxt="n",ylim=c(0,50))
 axis(2,at=c(0,25,50),cex.axis=2)
-abline(v=median(para[[14]][,1]),col="red")
+abline(v=median(para[[13]][,1]),col="red")
 legend("topright",lty=1,col="red",legend="median",cex=2,bty="n")
-hist(para[[14]][,2],breaks=55,main="",xlab=expression(p[r]),ylim=c(0,50),cex.axis=2,cex.lab=2,yaxt="n")
+hist(para[[13]][,2],breaks=80,main="",xlab=expression(p[f]),ylim=c(0,60),cex.axis=2,cex.lab=2,yaxt="n")
 axis(2,at=c(0,25,50),cex.axis=2)
-abline(v=median(para[[14]][,2]),col="red")
+abline(v=median(para[[13]][,2]),col="red")
 legend("topright",lty=1,col="red",legend="median",cex=2,bty="n")
 
 #correlation
-plot(para[[14]][,1],para[[14]][,2],xlab=expression(rho),ylab=expression(p[r]),main="",,cex.axis=1.5,cex.lab=1.5)
+plot(para[[13]][,1],para[[13]][,2],xlab=expression(rho),ylab=expression(p[f]),main="",,cex.axis=2,cex.lab=2)
 
+##HDPI
+hdi(para[[13]][,1], credMass = 0.95)  # credMass = 0.95 表示95%置信区间
+median(para[[13]][,1])
 
--- Death rate.R
+++ Death rate.R
@@ -1,18 +1,24 @@
-##Appendix S1
-
 Infected_data <- read.csv("Infected_mosquito.csv")
 Uninfected_data <- read.csv("Uninfected_mosquito.csv")
 data <- cbind(rbind(Infected_data,Uninfected_data),c(rep(1,17),rep(0,2)))
-ft <- -log(data[,2])/data[,1]
-data <- cbind(data,ft)
-names(data) <- c("Time","Percentage","Infected","Transform")
+names(data) <- c("Time","Percentage","Infected")
 
-Exp_nls <- nls(Transform ~ a*exp(b*Time+c*Infected),data = data ,start=list(a=0.1,b=0.1,c=0.1))
+
+library(minpack.lm)
+
+# Try using the 'nlsLM' function from minpack.lm for a more robust fit
+Exp_nls <- nlsLM(Percentage ~ exp(-a/b * exp(c*Infected)*(exp(b*Time)-1)),
+                 data = data,
+                 start = list(a = 0.1, b = 0.1, c = 0.1))
+
 summary(Exp_nls)
+a <- 0.0050300 
+b <- 0.1326802
+c <- 0.3272420
 
-plot(data[1:17,1],exp(-0.0043140*data[1:17,1]*exp(0.0874910*data[1:17,1]+0.3604109)),type="l",xlab="Time post blood feed (Days)",ylab="Mosquito survival",ylim=c(0,1),yaxt="n",cex.axis=1.2,cex.lab=1.2,cex.main=1.2,col="red")
+plot(data[1:17,1],exp(-a/b * exp(c*data[1:17,3])*(exp(b*data[1:17,1])-1)),type="l",xlab="Time post blood feed (Days)",ylab="Mosquito survival",ylim=c(0,1),yaxt="n",cex.axis=1.2,cex.lab=1.2,cex.main=1.2,col="red")
 axis(2,at=c(0,0.2,0.4,0.6,0.8,1) ,lab=paste0(c(0,0.2,0.4,0.6,0.8,1) * 100,"%"),las=TRUE,cex.axis=1.2)
 points(data[1:17,1],data[1:17,2],col="red")
-lines(data[1:17,1],exp(-0.0043140*data[1:17,1]*exp(0.0874910*data[1:17,1])),col="blue")
+lines(data[1:17,1],exp(-a/b*(exp(b*data[1:17,1])-1)),col="blue")
 points(data[18:19,1],data[18:19,2],col="blue")
 legend("topright",col=c("blue","red"),lty=1,legend=c("Uninfected","Infected"))
